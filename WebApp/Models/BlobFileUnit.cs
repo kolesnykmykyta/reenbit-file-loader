5b54056fc01b9d0230737ab4fa6c50f428770121
Mykyta Kolesnyk <kolesnik23215@gmail.com>
2024-03-07 16:54:36 +0200

    Change form to the EditForm Blazor component

---
+++ WebApp/Models/BlobFileUnit.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace WebApp.Models
+{
+    public class BlobFileUnit
+    {
+        public IBrowserFile? File { get; set; }
+
+        public string? Email { get; set; }
+    }
+}
