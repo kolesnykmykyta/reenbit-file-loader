5ed8aaf5fdc62d4affbee93d39a08a6c454f51c2
Mykyta Kolesnyk <kolesnik23215@gmail.com>
2024-03-09 13:13:51 +0200

    Expand BlobStorage functionality

--- Infrastructure/BlobAccess/BlobStorage.cs
+++ Infrastructure/BlobAccess/BlobStorage.cs
@@ -1,5 +1,7 @@
 ﻿using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
+using Azure.Storage.Sas;
+using Azure.Storage;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,6 +30,32 @@ namespace Infrastructure.BlobAccess
             };
 
             await blobClient.UploadAsync(file, options);
+        }
+
+        public string GenerateSasToken(string? fileName, string? accesKey, int expireTime)
+        {
+            string storageName = _serviceClient.AccountName;
+            string containerName = _containerClient.Name;
+
+            BlobSasBuilder builder = new BlobSasBuilder()
+            {
+                BlobContainerName = containerName,
+                BlobName = fileName,
+                ExpiresOn = DateTime.Now.AddSeconds(expireTime),
+            };
+            builder.SetPermissions(BlobSasPermissions.Read);
+
+            StorageSharedKeyCredential credential = new(storageName, accesKey);
+            string sasToken = builder.ToSasQueryParameters(credential).ToString();
+
+            return sasToken;
+        }
+
+        public BlobProperties GetBlobMetadata(string? fileName)
+        {
+            BlobClient blobClient = _containerClient.GetBlobClient(fileName);
+
+            return blobClient.GetProperties();
         }
     }
 }
--- Infrastructure/BlobAccess/IBlobStorage.cs
+++ Infrastructure/BlobAccess/IBlobStorage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Azure.Storage.Blobs.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,9 @@ namespace Infrastructure.BlobAccess
     public interface IBlobStorage
     {
         public Task UploadFileAsync(Stream file, string fileName, Dictionary<string, string>? metadata = null);
+
+        public string GenerateSasToken(string? fileName, string? accessKey, int expireTime);
+
+        public BlobProperties GetBlobMetadata(string? fileName);
     }
 }
