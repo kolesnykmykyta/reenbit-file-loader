db0da404fb220304afdcb74fa4b7a6c6bf43482a
Mykyta Kolesnyk <kolesnik23215@gmail.com>
2024-03-10 14:06:23 +0200

    Add unit tests for the new BlobStorageService functionality

--- Infrastructure.Tests/Services/BlobStorageServiceTests.cs
+++ Infrastructure.Tests/Services/BlobStorageServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using Infrastructure.BlobAccess;
+﻿using Azure;
+using Infrastructure.BlobAccess;
 using Infrastructure.Services;
 using Moq;
 using System;
@@ -52,8 +53,9 @@ namespace Infrastructure.Tests.Services
             string expectedParamName = "originalName";
 
             var exception = await Assert.ThrowsAsync<ArgumentException>(() => service.UploadFileToStorageAsync(mockStream.Object, originalName, "test@email"));
+            string? actualParamName = exception.ParamName;
 
-            Assert.Equal(expectedParamName, exception.ParamName);
+            Assert.Equal(expectedParamName, actualParamName);
         }
 
         [Theory]
@@ -67,8 +69,9 @@ namespace Infrastructure.Tests.Services
             string expectedParamName = "file";
 
             var exception = await Assert.ThrowsAsync<ArgumentException>(() => service.UploadFileToStorageAsync(mockStream.Object, originalName, "test@email"));
+            string? actualParamName = exception.ParamName;
 
-            Assert.Equal(expectedParamName, exception.ParamName);
+            Assert.Equal(expectedParamName, actualParamName);
         }
 
         [Theory]
@@ -81,8 +84,9 @@ namespace Infrastructure.Tests.Services
             string expectedParamName = "email";
 
             var exception = await Assert.ThrowsAsync<ArgumentException>(() => service.UploadFileToStorageAsync(mockStream.Object, "test.docx", email));
+            string? actualParamName = exception.ParamName;
 
-            Assert.Equal(expectedParamName, exception.ParamName);
+            Assert.Equal(expectedParamName, actualParamName);
         }
 
         [Fact]
@@ -92,8 +96,73 @@ namespace Infrastructure.Tests.Services
             string expectedParamName = "file";
 
             var exception = await Assert.ThrowsAsync<ArgumentNullException>(() => service.UploadFileToStorageAsync(null, "test.docx", "test@email"));
+            string? actualParamName = exception.ParamName;
 
-            Assert.Equal(expectedParamName, exception.ParamName);
+            Assert.Equal(expectedParamName, actualParamName);
+        }
+
+        [Fact]
+        public void GetFileUrl_CorrectValues_ReturnsFileUrl()
+        {
+            string fileName = "test.docx";
+            string accessKey = "access-key";
+            string storageName = "storage-name";
+            string containerName = "container-name";
+            string sasToken = "sas-token";
+            var blobStorageMock = new Mock<IBlobStorage>();
+            blobStorageMock.Setup(x => x.ContainerName)
+                .Returns(containerName);
+            blobStorageMock.Setup(x => x.StorageName)
+                .Returns(storageName);
+            blobStorageMock.Setup(x => x.GenerateSasToken(fileName, accessKey, 3600))
+                .Returns(sasToken);
+            string expected = $"https://{storageName}.blob.core.windows.net/{containerName}/{fileName}?{sasToken}";
+            BlobStorageService service = new BlobStorageService(blobStorageMock.Object);
+
+            string actual = service.GetFileUrl(fileName, accessKey);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void GetFileUrl_InvalidFileName_ThrowsArgumentException(string? fileName)
+        {
+            BlobStorageService service = new BlobStorageService(new Mock<IBlobStorage>().Object);
+            string expectedParamName = "fileName";
+
+            var exception = Assert.Throws<ArgumentException>(() => service.GetFileUrl(fileName, "access-key"));
+            string? actualParamName = exception.ParamName;
+
+            Assert.Equal(expectedParamName, actualParamName);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void GetFileUrl_InvalidAccessKey_ThrowsArgumentException(string? accessKey)
+        {
+            BlobStorageService service = new BlobStorageService(new Mock<IBlobStorage>().Object);
+            string expectedParamName = "accessKey";
+
+            var exception = Assert.Throws<ArgumentException>(() => service.GetFileUrl("test.docx", accessKey));
+            string? actualParamName = exception.ParamName;
+
+            Assert.Equal(expectedParamName, actualParamName);
+        }
+
+        [Fact]
+        public void GetFileUrl_WrongAccessKey_ThrowsInvalidOperationException()
+        {
+            var blobStorageMock = new Mock<IBlobStorage>();
+            blobStorageMock.Setup(x => x.GenerateSasToken(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>()))
+                .Throws(new RequestFailedException("Error Message"));
+
+            BlobStorageService service = new BlobStorageService(blobStorageMock.Object);
+
+            Assert.Throws<InvalidOperationException>(() => service.GetFileUrl("text.docx", "invalidkey"));
         }
     }
 }
